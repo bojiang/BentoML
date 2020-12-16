baff0bd38dab814bfffce664c2e3eb35b3036c2f
bojiang <hrmthw@outlook.com>
2020-12-16 20:20:08 +0800

    add test for customized route

--- tests/integration/api_server/test_meta.py
+++ tests/integration/api_server/test_meta.py
@@ -1,4 +1,6 @@
 # pylint: disable=redefined-outer-name
+import json
+
 import pytest
 
 
@@ -7,3 +9,14 @@ async def test_api_server_meta(host):
     await pytest.assert_request("GET", f"http://{host}/")
     await pytest.assert_request("GET", f"http://{host}/healthz")
     await pytest.assert_request("GET", f"http://{host}/docs.json")
+
+
+@pytest.mark.asyncio
+async def test_customized_route(host):
+    await pytest.assert_request(
+        "POST",
+        f"http://{host}/$~!@%^&*()_-+=[]\\|;:,./predict_json",
+        headers=(("Content-Type", "application/json"),),
+        data=json.dumps("hello"),
+        assert_data=bytes('"hello"', 'ascii'),
+    )
